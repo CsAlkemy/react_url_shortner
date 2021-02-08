0be03efab6025d890b523d530d4d6ac8d600ed88
CsAlkemy <alkemy48@gmail.com>
2021-02-08 16:04:34 +0600

    ui done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import "./App.css";
+import { BiLinkAlt } from "react-icons/bi";
+import heroImage from '../src/images/hero.png';
 
 function App() {
   return (
-    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-5">
-      <div className="md:flex">
-        <div className="md:flex-shrink-0">
-          <img
-            className="h-48 w-full object-cover md:w-48"
-            src="https://images.unsplash.com/photo-1515711660811-48832a4c6f69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
-            alt="Man looking at item at a store"
-          ></img>
-        </div>
-        <div className="p-8">
-          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-            Case study
-          </div>
-          <p className="mt-2 text-gray-500">
-            Getting a new business off the ground is a lot of hard work. Here
-            are five ideas you can use to find your first customers.
-          </p>
+    <>
+      <div className="container mx-auto max-w-7xl ">
+        <div className="flex nav p-5 pt-12">
+          <span className="text-colors-light">
+            <BiLinkAlt size={35} />
+          </span>
+          <h1 className="text-2xl font-semibold pt-2 pl-0">
+            URL<span className="font-extralight">shorter</span>{" "}
+          </h1>
+
+          <button className="bg-colors-green-100 ml-auto text-colors-light pl-4 pr-4">
+            {" "}
+            Buy me a cup of coffee
+          </button>
         </div>
       </div>
-    </div>
+      <div className="flex flex-col pl-5 pr-5">
+        <figure className='mt-0'>
+          <img  className='m-auto' src={heroImage} alt="hero"/>
+        </figure>
+        <h1 className="font-semibold m-auto text-6xl md:text-7xl">
+          Shorten Any Links
+          <span className=" font-semibold text-colors-light">.</span>
+        </h1>
+        <p className="m-auto">
+          Build and protect your brand using powerful and recognizable short
+          links. <span className="text-colors-light"> It's free forever.</span>{" "}
+        </p>
+        <div className="flex col-span-1 mx-auto">
+          <input
+            className="mt-7 mb-5 pl-3 md:pl-16 focus:ring-4 rounded-md focus:ring-offset-colors-light bg-colors-gray-100 max-w-lg text-colors-gray-600 p-3 pr-10"
+            placeholder="Type or paste your link"
+          />
+          <button className="bg-colors-light font-medium ml-3 mt-7 mb-5 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 text-colors-white pl-4 pr-4">
+            {" "}
+            Shorten
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      colors: {
+        light: "#00adb4",
+        transparent: "transparent",
+        current: "currentColor",
+        black: colors.black,
+        white: colors.white,
+        gray: colors.trueGray,
+        indigo: colors.indigo,
+        red: colors.rose,
+        yellow: colors.amber,
+        pink:colors.pink,
+        violet:colors.violet,
+        blue:colors.blue,
+        green:colors.emerald,
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
